37964c1fe621ad895079bff7837e86f81192584f
Tudor <tudor.paun76@gmail.com>
2024-03-31 19:15:05 +0100

    final secure push

--- register.php
+++ register.php
@@ -5,16 +5,21 @@ if(isset($_POST['register'])){
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
     $isAdmin = isset($_POST['isAdmin']) && $_POST['isAdmin'] === 'yes' ? 1 : 0;
 
     $query = "INSERT INTO users (username, password, isAdmin) VALUES (:username, :password, :isAdmin)";
     $stmt = $db->prepare($query);
     $stmt->bindParam(':username', $username);
-    $stmt->bindParam(':password', $password);
+    $stmt->bindParam(':password', $hashed_password);
     $stmt->bindParam(':isAdmin', $isAdmin);
     $stmt->execute();
 }
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -36,6 +41,13 @@ if(isset($_POST['register'])){
     <div class="form-group">
         <input type="password" name="password" class="form-control" placeholder="Password" required>
     </div>
+    <div class="form-group">
+        <label for="isAdmin">Register as Admin?</label><br>
+        <input type="radio" id="isAdminYes" name="isAdmin" value="yes">
+        <label for="isAdminYes">Yes</label>
+        <input type="radio" id="isAdminNo" name="isAdmin" value="no" checked>
+        <label for="isAdminNo">No</label>
+    </div>
     <button type="submit" name="register" class="btn btn-primary">Register</button>
 </form>
 
@@ -43,6 +55,7 @@ if(isset($_POST['register'])){
         <p class="mt-3">Return to login <a href="login.php">Login</a></p>
     </div>
 
+    <!-- Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
